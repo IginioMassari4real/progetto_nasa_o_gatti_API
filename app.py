4c4a67adf2d83e1ebc0b7d4892c329c2338701bc
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2025-01-14 08:50:08 +0000

    routes funzionanti

--- app.py
+++ app.py
@@ -10,14 +10,17 @@ def nasa_home():
     response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}')
     #richiedo i dati
     data = response.json() #converte da json a dict python
-    return render_template('nasa.html', title=data.get("title"),
-description=data.get("explanation"), image_url=data.get("url")),
+    return render_template('nasa.html', title=data.get("title"), description=data.get("explanation"), image_url=data.get("url"))
 
 @app.route('/cat')
 def cat_home():
     response = requests.get('https://catfact.ninja/fact') #richiedo i dati
     data = response.json() #converte da json a dict python
     return render_template('cat_fact.html', fact=data.get("fact"))
+
+@app.route('/cat/change', methods=['POST'])
+def cat_change():
+    return redirect(url_for('cat_home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
